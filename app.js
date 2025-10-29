b66acaec75ab408aee96fa50d7d1ce0ddcd3ccd4
Sander Sooväli <sander.soovali@voco.ee>
2025-10-29 08:49:55 +0200

    Created project basic files

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('express test');
+});
+
+app.listen(3015, () => {
+  console.log('Server us running');
+});
